e3dd4ba97d4da833e467901c81df5f79a67ac909
Olexii Petyxachnyi <leshalol66@gmail.com>
2024-04-23 04:34:05 +0300

    password for administration and for user

--- main.cpp
+++ main.cpp
@@ -60,184 +60,330 @@ int main() {
     Balance *array[] ={withdraw , deposit};
 
     int choice, choice2, choice3, choice4;
-
+    int password;
     do {
-        cout << " Please choose which animal you want take in your house \n";
-        cout << " 1. DOG \n";
-        cout << " 2. CAT \n";
-        cout << " 3. RABBIT \n";
-        cout << " 4. OUR EMPLOYEES\n";
-        cout << " 5. BUYERS\n";
-        cout << " 6. YOUR CASHBOX | NEWS\n";
-        cout << " 7. EXIT \n";
-
-        cin >> choice;
-
-        switch (choice) {
-            case 1 :
-
-                cout << " Which one you want to take ?\n";
-                cout << endl;
-
-                cout << "1.\n";
-                doberman.print();
-                cout << endl;
-
-                cout << "2.\n";
-                german_shepherd.print();
-                cout << endl;
-
-                cout << "3.\n";
-
-                other.getName();
-                other.setName("Vasiliy");
-
-                other.getSize();
-                other.setSize(62);
-
-                other.print();
-
-                //siberian_husky.print();
-
-                cout << endl;
-
-                cin >> choice2;
-                switch (choice2) {
-                    case 1 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    case 2 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    case 3 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    default:
-                        cout << " We don't have these Dog !\n";
-                        break;
-                }
-                break;
-
-            case 2:
-                cout << "Which one you want to pick ?\n";
-
-                cout << "1.\n";
-                Cat().print();
-                cout << endl;
-
-                cout << "2.\n";
-                copy.print();
-                cout << endl;
-
-                cout << "3.\n";
-                special_cat.print();
-                cout << endl;
-
-                cin >> choice3;
-
-                switch (choice3) {
-                    case 1 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    case 2 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    case 3 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    default:
-                        cout << " We don't have these Cat !\n";
-                        break;
-                }
-                break;
-
-            case 3:
-                cout << " Which one you want to pick up ?\n";
-
-                cout << endl;
-
-                cout << "1.\n";
-                special_rabbit.print();
-                cout << endl;
-
-                cout << "2.\n";
-                Rabbit::setBehavior("Calm");
-                common_rabbit.print();
-                cout << endl;
-
-                cout << "3.\n";
-                Rabbit::setBehavior("Running");
-                middle_rabbit.print();
-
-                cout << endl;
-
-                cin >> choice4;
-
-                switch (choice4) {
-                    case 1 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    case 2 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    case 3 :
-                        cout << " Thanks for buying !\n";
-                        break;
-
-                    default:
-                        cout << " We don't have these Rabbit !\n";
-                        break;
-                }
-                break;
-            case 4:
-                first_employee.Anton();
-                first_employee.Print();
-                cout << endl;
-
-                second_emloyee.Masha();
-                second_emloyee.Print();
-                cout << endl;
-
-                third_employee.Masha();
-                third_employee.Print();
-                cout << endl;
-                break;
-
-            case 5:
-                buyer.Print();
-                cout << endl;
-                cout << endl;
-                break;
-
-            case 6:
-                Till cashbox;
-                cashbox.cashbox();
-                cashbox.print();
-                cout << endl;
-
-                Human stufff;
-                Employee second_stuff;
-
-                stufff.doSomeStuff();
-                second_stuff.doSomeStuff();
-
-                Human *pb = new Employee;
-                pb->doSomeStuff();
-
-                cout << endl;
-                break;
-
+        cout << "Please enter the password (0 or 1):" << endl;
+        cin >> password;
+        if (password != 0 && password != 1) {
+            cout << "Invalid password! Please enter 0 or 1." << endl;
         }
-    } while (choice != 7);
+    } while (password != 0 && password != 1);
 
+    if (password == 1){
+        do {
+            cout << " Please choose which animal you want take in your house \n";
+            cout << " 1. DOG \n";
+            cout << " 2. CAT \n";
+            cout << " 3. RABBIT \n";
+            cout << " 4. EXIT \n";
+
+            cin >> choice;
+
+            switch (choice) {
+                case 1 :
+
+                    cout << " Which one you want to take ?\n";
+                    cout << endl;
+
+                    cout << "1.\n";
+                    doberman.print();
+                    cout << endl;
+
+                    cout << "2.\n";
+                    german_shepherd.print();
+                    cout << endl;
+
+                    cout << "3.\n";
+
+                    other.getName();
+                    other.setName("Vasiliy");
+
+                    other.getSize();
+                    other.setSize(62);
+
+                    other.print();
+
+                    //siberian_husky.print();
+
+                    cout << endl;
+
+                    cin >> choice2;
+                    switch (choice2) {
+                        case 1 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 2 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 3 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        default:
+                            cout << " We don't have these Dog !\n";
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    cout << "Which one you want to pick ?\n";
+
+                    cout << "1.\n";
+                    Cat().print();
+                    cout << endl;
+
+                    cout << "2.\n";
+                    copy.print();
+                    cout << endl;
+
+                    cout << "3.\n";
+                    special_cat.print();
+                    cout << endl;
+
+                    cin >> choice3;
+
+                    switch (choice3) {
+                        case 1 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 2 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 3 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        default:
+                            cout << " We don't have these Cat !\n";
+                            break;
+                    }
+                    break;
+
+                case 3:
+                    cout << " Which one you want to pick up ?\n";
+
+                    cout << endl;
+
+                    cout << "1.\n";
+                    special_rabbit.print();
+                    cout << endl;
+
+                    cout << "2.\n";
+                    Rabbit::setBehavior("Calm");
+                    common_rabbit.print();
+                    cout << endl;
+
+                    cout << "3.\n";
+                    Rabbit::setBehavior("Running");
+                    middle_rabbit.print();
+
+                    cout << endl;
+
+                    cin >> choice4;
+
+                    switch (choice4) {
+                        case 1 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 2 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 3 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        default:
+                            cout << " We don't have these Rabbit !\n";
+                            break;
+                    }
+                    break;
+                }
+        } while (choice != 4);
+
+    }else{
+        do {
+            cout << " Please choose which animal you want take in your house \n";
+            cout << " 1. DOG \n";
+            cout << " 2. CAT \n";
+            cout << " 3. RABBIT \n";
+            cout << " 4. OUR EMPLOYEES\n";
+            cout << " 5. BUYERS\n";
+            cout << " 6. YOUR CASHBOX | NEWS\n";
+            cout << " 7. EXIT \n";
+
+            cin >> choice;
+
+            switch (choice) {
+                case 1 :
+
+                    cout << " Which one you want to take ?\n";
+                    cout << endl;
+
+                    cout << "1.\n";
+                    doberman.print();
+                    cout << endl;
+
+                    cout << "2.\n";
+                    german_shepherd.print();
+                    cout << endl;
+
+                    cout << "3.\n";
+
+                    other.getName();
+                    other.setName("Vasiliy");
+
+                    other.getSize();
+                    other.setSize(62);
+
+                    other.print();
+
+                    //siberian_husky.print();
+
+                    cout << endl;
+
+                    cin >> choice2;
+                    switch (choice2) {
+                        case 1 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 2 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 3 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        default:
+                            cout << " We don't have these Dog !\n";
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    cout << "Which one you want to pick ?\n";
+
+                    cout << "1.\n";
+                    Cat().print();
+                    cout << endl;
+
+                    cout << "2.\n";
+                    copy.print();
+                    cout << endl;
+
+                    cout << "3.\n";
+                    special_cat.print();
+                    cout << endl;
+
+                    cin >> choice3;
+
+                    switch (choice3) {
+                        case 1 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 2 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 3 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        default:
+                            cout << " We don't have these Cat !\n";
+                            break;
+                    }
+                    break;
+
+                case 3:
+                    cout << " Which one you want to pick up ?\n";
+
+                    cout << endl;
+
+                    cout << "1.\n";
+                    special_rabbit.print();
+                    cout << endl;
+
+                    cout << "2.\n";
+                    Rabbit::setBehavior("Calm");
+                    common_rabbit.print();
+                    cout << endl;
+
+                    cout << "3.\n";
+                    Rabbit::setBehavior("Running");
+                    middle_rabbit.print();
+
+                    cout << endl;
+
+                    cin >> choice4;
+
+                    switch (choice4) {
+                        case 1 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 2 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        case 3 :
+                            cout << " Thanks for buying !\n";
+                            break;
+
+                        default:
+                            cout << " We don't have these Rabbit !\n";
+                            break;
+                    }
+                    break;
+                case 4:
+                    first_employee.Anton();
+                    first_employee.Print();
+                    cout << endl;
+
+                    second_emloyee.Masha();
+                    second_emloyee.Print();
+                    cout << endl;
+
+                    third_employee.Masha();
+                    third_employee.Print();
+                    cout << endl;
+                    break;
+
+                case 5:
+                    buyer.Print();
+                    cout << endl;
+                    cout << endl;
+                    break;
+
+                case 6:
+                    Till cashbox;
+                    cashbox.cashbox();
+                    cashbox.print();
+                    cout << endl;
+
+                    Human stufff;
+                    Employee second_stuff;
+
+                    stufff.doSomeStuff();
+                    second_stuff.doSomeStuff();
+
+                    Human *pb = new Employee;
+                    pb->doSomeStuff();
+
+                    cout << endl;
+                    break;
+
+            }
+        } while (choice != 7);
+    }
 
     delete[] &array;
     return 0;
